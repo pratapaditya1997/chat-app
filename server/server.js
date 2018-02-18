1403b218523a5d733a5126402da180a1db9a4b57
wh1t3_r0s3 <pratapaditya1997@gmail.com>
2018-02-18 13:17:48 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
+  //
+  socket.emit('newMessage',{
+    from: 'admin',
+    text: 'Welcome to the chat room',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from: 'admin',
+    text: 'new user has joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
     io.emit('newMessage',{
